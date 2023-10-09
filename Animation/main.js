5c3292a38158a8e36ed107a16df215df05104687
Yeffer Moreno Robledo <yefire.js@gmail.com>
2023-10-09 13:04:39 -0500

    upda

--- Animation/main.js
+++ Animation/main.js
@@ -1,7 +1,12 @@
 const jordan = document.querySelector('#jordan');
 const modal = document.querySelector('.modal');
+const closee = document.querySelector('.modal__content--close');
 
 jordan.addEventListener('click',  ()=> {
     modal.classList.remove('hidden')
     modal.classList.add('visible')
+})
+closee.addEventListener('click',  ()=> {
+    modal.classList.remove('visible')
+    modal.classList.add('hidden')
 })
